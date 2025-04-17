726778737fbaa841d7313439d0e66cc06872f9b1
Nihillux <oan.mkoll@gmail.com>
2025-04-17 23:29:52 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import { useState } from 'react';
 import Log from './components/Log.jsx'
 import { WINNING_COMBINATIONS } from './components/winning-combinations.js';
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+]
+
 function derivedActivePlayer(gameTurns) {
   let currentPlayer = 'X';
 
@@ -19,6 +25,27 @@ function App() {
   // const [activePlayer, setActivePlayer] = useState ('X');
 
  const activePlayer = derivedActivePlayer(gameTurns);
+
+ let gameBoard = initialGameBoard;
+
+    for (const turn of gameTurns) {
+        const { square, player } = turn;
+        const {row, col } = square;
+
+        gameBoard[row][col] = player;
+        }
+
+let winner;
+
+ for (const combination of WINNING_COMBINATIONS) {
+  const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+  const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+  const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+  if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+    winner = firstSquareSymbol;
+  }
+ }
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
@@ -38,7 +65,8 @@ function App() {
        <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
        <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
       </ol>
-        <Gameboard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+      {winner && <p>You won, {winner}</p>}
+        <Gameboard onSelectSquare={handleSelectSquare} board={gameBoard} />
     </div>
     <Log turns={gameTurns}/>
   </main>;
